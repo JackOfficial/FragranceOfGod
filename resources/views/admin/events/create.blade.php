dc98e9858a0ca99197362c098172740f708d368d
JackOfficial <musengimanajacques@gmail.com>
2026-01-04 23:44:03 +0200

    Event Pages

--- resources/views/admin/events/create.blade.php
+++ resources/views/admin/events/create.blade.php
@@ -24,77 +24,100 @@
                 <form action="{{ route('admin.events.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
 
-                    <div class="row">
-                        <!-- Left Column -->
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="title">Event Title <span class="text-danger">*</span></label>
-                                <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}" required>
+                    <!-- Event Info -->
+                    <fieldset class="border p-3 mb-3">
+                        <legend class="w-auto px-2"><i class="fas fa-info-circle"></i> Event Info</legend>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="title"><i class="fas fa-heading"></i> Event Title <span class="text-danger">*</span></label>
+                                    <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}" required>
+                                </div>
                             </div>
 
-                            <div class="form-group">
-                                <label for="event_date">Event Date <span class="text-danger">*</span></label>
-                                <input type="date" name="event_date" id="event_date" class="form-control" value="{{ old('event_date') }}" required>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="event_date"><i class="fas fa-calendar-alt"></i> Event Date <span class="text-danger">*</span></label>
+                                    <input type="date" name="event_date" id="event_date" class="form-control" value="{{ old('event_date') }}" required>
+                                </div>
                             </div>
 
-                            <div class="form-group">
-                                <label for="location">Location <span class="text-danger">*</span></label>
-                                <input type="text" name="location" id="location" class="form-control" value="{{ old('location') }}" required>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="location"><i class="fas fa-map-marker-alt"></i> Location <span class="text-danger">*</span></label>
+                                    <input type="text" name="location" id="location" class="form-control" value="{{ old('location') }}" required>
+                                </div>
                             </div>
 
-                            <div class="form-group">
-                                <label for="is_published">Publish Event?</label>
-                                <select name="is_published" id="is_published" class="form-control">
-                                    <option value="1" {{ old('is_published') == 1 ? 'selected' : '' }}>Yes</option>
-                                    <option value="0" {{ old('is_published') == 0 ? 'selected' : '' }}>No</option>
-                                </select>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="is_published"><i class="fas fa-eye"></i> Publish Event?</label>
+                                    <select name="is_published" id="is_published" class="form-control">
+                                        <option value="1" {{ old('is_published') == 1 ? 'selected' : '' }}>Yes</option>
+                                        <option value="0" {{ old('is_published') == 0 ? 'selected' : '' }}>No</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
-
-                        <!-- Right Column -->
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="images">Event Images</label>
-                                <input type="file" name="images[]" id="images" class="form-control" accept="image/*" multiple
-                                       @change="imagePreviews = Array.from($event.target.files).map(f => URL.createObjectURL(f))">
-                                <template x-if="imagePreviews.length">
-                                    <div class="mt-2 d-flex flex-wrap gap-2">
-                                        <template x-for="src in imagePreviews" :key="src">
-                                            <img :src="src" class="img-thumbnail" style="width:100px; height:100px; object-fit:cover;">
-                                        </template>
-                                    </div>
-                                </template>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="documents">Attach Documents / PDFs</label>
-                                <input type="file" name="documents[]" id="documents" multiple class="form-control"
-                                       @change="docs = Array.from($event.target.files).map(f => ({ name: f.name, size: f.size }))">
-                                <template x-if="docs.length">
-                                    <ul class="list-group mt-2">
-                                        <template x-for="(doc, index) in docs" :key="index">
-                                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                <span x-text="doc.name"></span>
-                                                <small class="text-muted" x-text="'(' + Math.round(doc.size/1024) + ' KB)'"></small>
-                                            </li>
-                                        </template>
-                                    </ul>
-                                </template>
-                            </div>
-                        </div>
-                    </div>
+                    </fieldset>
 
                     <!-- Description -->
-                    <div class="form-group mt-3">
-                        <label for="description">Description <span class="text-danger">*</span></label>
-                        <textarea name="description" id="description" class="form-control summernote">{{ old('description') }}</textarea>
-                    </div>
+                    <fieldset class="border p-3 mb-3">
+                        <legend class="w-auto px-2"><i class="fas fa-align-left"></i> Description</legend>
+                        <div class="form-group">
+                            <textarea name="description" id="description" class="form-control summernote">{{ old('description') }}</textarea>
+                        </div>
+                    </fieldset>
 
-                    <!-- Submit -->
-                    <div class="d-flex justify-content-end mt-4 gap-2">
+                    <!-- Media Uploads -->
+                    <fieldset class="border p-3 mb-3">
+                        <legend class="w-auto px-2"><i class="fas fa-file-upload"></i> Media Uploads</legend>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <!-- Event Images -->
+                                <div class="form-group">
+                                    <label for="images"><i class="fas fa-image"></i> Event Images</label>
+                                    <input type="file" name="images[]" id="images" accept="image/*" class="form-control" multiple
+                                           @change="imagePreviews = Array.from($event.target.files).map(f => URL.createObjectURL(f))">
+
+                                    <template x-if="imagePreviews.length">
+                                        <div class="mt-2 d-flex flex-wrap gap-2">
+                                            <template x-for="src in imagePreviews" :key="src">
+                                                <img :src="src" class="img-thumbnail" style="width:100px; height:100px; object-fit:cover;">
+                                            </template>
+                                        </div>
+                                    </template>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <!-- Event Documents -->
+                                <div class="form-group">
+                                    <label for="documents"><i class="fas fa-file-pdf"></i> Attach Documents / PDFs</label>
+                                    <input type="file" name="documents[]" id="documents" multiple class="form-control"
+                                           @change="docs = Array.from($event.target.files).map(f => ({ name: f.name, size: f.size }))">
+
+                                    <template x-if="docs.length">
+                                        <ul class="list-group mt-2">
+                                            <template x-for="(doc, index) in docs" :key="index">
+                                                <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                    <span x-text="doc.name"></span>
+                                                    <small class="text-muted" x-text="'(' + Math.round(doc.size/1024) + ' KB)'"></small>
+                                                </li>
+                                            </template>
+                                        </ul>
+                                    </template>
+                                </div>
+                            </div>
+                        </div>
+                    </fieldset>
+
+                    <!-- Submit Buttons -->
+                    <div class="d-flex justify-content-end gap-2">
                         <a href="{{ route('admin.events.index') }}" class="btn btn-secondary"><i class="fas fa-times"></i> Cancel</a>
                         <button type="submit" class="btn btn-success"><i class="fas fa-plus-circle"></i> Create Event</button>
                     </div>
+
                 </form>
             </div>
         </div>
