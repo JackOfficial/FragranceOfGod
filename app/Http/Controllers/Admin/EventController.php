9f0b51ca00d44679e7217120b1ac874f42b6af1c
JackOfficial <musengimanajacques@gmail.com>
2026-01-04 23:10:29 +0200

    Event Pages

--- app/Http/Controllers/Admin/EventController.php
+++ app/Http/Controllers/Admin/EventController.php
@@ -17,7 +17,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        $events = Event::latest()->paginate(10);
+         $events = Event::with('media')->latest()->paginate(10); 
         return view('admin.events.index', compact('events'));
     }
 
