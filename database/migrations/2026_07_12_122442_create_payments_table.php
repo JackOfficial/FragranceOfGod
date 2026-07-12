e00bc45bc9d94ed8e6e07313e859d87e649e462b
JackOfficial <musengimanajacques@gmail.com>
2026-07-12 15:07:22 +0200

    Editing and quick fixes

--- database/migrations/2026_07_12_122442_create_payments_table.php
+++ database/migrations/2026_07_12_122442_create_payments_table.php
@@ -12,15 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('payments', function (Blueprint $table) {
-               $table->id();
-        $table->string('transaction_ref')->unique();
-        $table->string('email')->nullable();
-        $table->string('phone_number');
-        $table->decimal('amount', 10, 2);
-        $table->string('currency', 4)->default('RWF');
-        $table->string('payment_method'); // e.g., mtn_rw, airtel_rw
-        $table->string('status')->default('PENDING'); // PENDING, SUCCESS, FAILED
-        $table->timestamps();
+            $table->id();
+            $table->string('transaction_ref')->unique();
+            $table->string('donor_name');
+            $table->string('email')->nullable();
+            $table->string('phone_number')->nullable(); // Nullable to safely accommodate Card checkouts
+            $table->decimal('amount', 10, 2);
+            $table->string('currency', 4)->default('RWF');
+            $table->string('payment_method'); // mtn_rw, airtel_rw, card
+            $table->text('message')->nullable();
+            $table->string('status')->default('PENDING'); // PENDING, SUCCESS, FAILED
+            $table->timestamps();
         });
     }
 
